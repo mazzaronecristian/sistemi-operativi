7fff512bdb76bafa9530447e644d81101aca87f6
mazzaronecristian <cristian.mazzarone@stud.unifi.it>
2023-06-02 16:42:30 +0200

    ulitmi ritocchi all'esercizio del compito del 21 luglio 2020

--- CompitiSistemiOperativi/src/Compito21072020/Compito21072020.java
+++ CompitiSistemiOperativi/src/Compito21072020/Compito21072020.java
@@ -8,18 +8,26 @@ public class Compito21072020 {
     public static void main (String[] args) throws InterruptedException{
         int M = 4;      //* Numero di stanze
         int K = 3;      //* Numero di stanze da visitare a volta
-        int N = 5;     //* Numero di persone
+        int N = 5;      //* Numero di persone
         GestoreStanze gs = new GestoreStanze(M);
         Persona[] p = new Persona[N];
         for (int i = 0; i < N; i++){
             p[i] = new Persona(gs, K);
+            p[i].setName("P"+i);
             p[i].start();
         }
 
         Thread.sleep(30000);
         for (int i = 0; i < N; i++){
             p[i].interrupt();
+            p[i].join();
         }
+
+        for (int i = 0; i < M; i++)
+            System.out.println("P"+i+" nVisits: "+p[i].nVisited);
+
+        for (int i = 0; i < M; i++)
+            System.out.println("Stanza "+i+" usata "+gs.used[i]+" volte");
     }
 }
 class GestoreStanze{
@@ -34,13 +42,22 @@ class GestoreStanze{
     }
 
     public int getStanzaMin(ArrayList<Integer> visited){
-        int pmin = -1;
+        int min = -1;
+        ArrayList<Integer> pmins = new ArrayList<>();
         for (int i = 0; i<used.length; i++){
-            if ( !visited.contains(i) && ( pmin == -1 || used[i] < used[pmin] ) )
-                pmin = i;
-        }
+            if (!visited.contains(i)){
+                if (min == -1 || used[i] < min) {
+                    min = used[i];
+                    pmins.clear();
+                    pmins.add(i);
+                } else if (used[i] == min)
+                    pmins.add(i);
+            }
 
-        return pmin;
+        }
+        int p = ( int )( Math.random()*pmins.size() );
+        System.out.println( Thread.currentThread().getName()+" mins: "+pmins+" scelgo: "+p );
+        return pmins.get(p);
     }
 
     public void acquireStanza(int st) throws InterruptedException {
@@ -55,12 +72,14 @@ class GestoreStanze{
 class Persona extends Thread{
     GestoreStanze gs;
     int K;
+    int nVisited;
 
     public Persona(GestoreStanze gs, int K) {
         this.gs = gs;
         this.K = K;
     }
 
+    @SuppressWarnings("InfiniteLoopStatement")
     public void run(){
         try {
             while (true){
@@ -69,11 +88,15 @@ class Persona extends Thread{
                     int st = gs.getStanzaMin(visited);
                     System.out.println(getName()+ " visita la stanza " + st+" "+visited);
                     gs.acquireStanza(st);
-                    System.out.println(getName()+ " sta usando la stanza " + st+" "+visited);
-                    visited.add(st);
-                    sleep(100);
-                    gs.releaseStanza(st);
-                    System.out.println(getName()+ " lascia la stanza " + st+" "+visited);
+                    try {
+                        nVisited++;
+                        System.out.println(getName()+ " sta usando la stanza " + st+" "+visited);
+                        visited.add(st);
+                        sleep(100);
+                    }finally {
+                        gs.releaseStanza(st);
+                        System.out.println(getName()+ " lascia la stanza " + st+" "+visited);
+                    }
                 }
             }
         } catch (InterruptedException e) {
